7664276dbb39fdcd78f1be299cb9b10b940fd395
bereketkurkura <bereketkurkura@gmail.com>
2022-07-12 13:17:14 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,23 +1,25 @@
 #include <unistd.h>
 #include "main.h"
-
 /**
  *_printf - takes in a string and prints different types of arguments for
  * an unspecified amount of arguments
  * @format: the initial string that tell us what is going to be printed
  * Return: the amount of times we write to stdout
  */
-
 int _printf(const char *format, ...)
-
 {
 	int i, count;
+
 	int (*f)(va_list);
+
 	va_list list;
+
 	if (format == NULL)
 		return (-1);
+
 	va_start(list, format);
 	i = count = 0;
+
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
