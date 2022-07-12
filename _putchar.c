2eeb0bec22e7a7e4b33ad934135433c2a878bb17
bereketkurkura <bereketkurkura@gmail.com>
2022-07-12 11:09:15 -0700

    0 and _putchar

---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ *_putchar - writes a character to stdout
+ *@c:charcter to print
+ *Return:1 - on success, -1 on error
+ */
+
+int _putchar(char c)
+
+{
+	write(1, &c, 1);
+	return (1);
+}
