568cfa4adf32adf623ae88edb77686d0c7fc3f53
Dagmawi-Demeke <dagimagna24@gmail.com>
2022-07-12 13:24:50 -0700

    edited

--- print_helper.c
+++ print_helper.c
@@ -21,7 +21,8 @@ char *rev_string(char *s)
 	while (half >= 0)
 	{
 		first = s[max - half];
-		last = s[half];									s[half] = first;
+		last = s[half];
+		s[half] = first;
 		s[max - half] = last;
 		half--;
 	}
