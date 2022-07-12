df7c801fde0267510fbd1283ab326e351bf02c4d
bereketkurkura <bereketkurkura@gmail.com>
2022-07-12 12:22:32 -0700

    print_r.c

---
+++ print_r.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_r - prints a string in reverse
+ *@list:list to increment
+ *Return:no. of charcters printed
+ */
+
+int print_r(va_list list)
+
+{
+	char *s = va_arg(list, char *);
+	int i, c;
+
+	if (s == NULL)
+		s = ")llun(";
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	i--;
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+		c++;
+	}
+	return (c);
+}
